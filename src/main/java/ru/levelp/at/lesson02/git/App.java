1d339b2180fa28047a5fd3da34a76bee2a1d6429
Khodakovskiy Dmitriy <khda91@gmail.com>
2023-02-02 21:41:04 +0300

    chages of examples

--- src/main/java/ru/levelp/at/lesson02/git/App.java
+++ src/main/java/ru/levelp/at/lesson02/git/App.java
@@ -17,11 +17,6 @@ public class App {
 
         // умножение
         System.out.println("10 * 12 = " + calculator.multiply(BigDecimal.valueOf(10), BigDecimal.valueOf(12)));
-        System.out.println("10 * 120 = " + calculator.multiply(BigDecimal.valueOf(10), BigDecimal.valueOf(120)));
-        System.out.println("10 * 10 = " + calculator.multiply(BigDecimal.valueOf(10), BigDecimal.valueOf(10)));
-        System.out.println("10 * 10 = " + calculator.multiply(BigDecimal.valueOf(10), BigDecimal.valueOf(10)));
-        System.out.println("10 * 10 = " + calculator.multiply(BigDecimal.valueOf(10), BigDecimal.valueOf(10)));
-        System.out.println("100 * 100 = " + calculator.multiply(BigDecimal.valueOf(100), BigDecimal.valueOf(100)));
         System.out.println("10 * 100 = " + calculator.multiply(BigDecimal.valueOf(100), BigDecimal.valueOf(100)));
         System.out.println("10 * 12 * 20 * 3 = "
                 + calculator.multiply(BigDecimal.valueOf(10), BigDecimal.valueOf(12),

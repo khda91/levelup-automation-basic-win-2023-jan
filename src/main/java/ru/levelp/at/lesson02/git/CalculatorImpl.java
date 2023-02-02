2266a5b6a13125e5f99f3ce556e7482901d8acaa
Khodakovskiy Dmitriy <khda91@gmail.com>
2023-02-02 20:25:30 +0300

    Revert "Commit calculatyor v2 and abs method in caclulator"
    This reverts commit 76328422472b7bfbfd1eccfa26201ad0b817c0b8.

--- src/main/java/ru/levelp/at/lesson02/git/CalculatorImpl.java
+++ src/main/java/ru/levelp/at/lesson02/git/CalculatorImpl.java
@@ -23,9 +23,4 @@ public class CalculatorImpl implements Calculator {
     public BigDecimal divide(BigDecimal a, BigDecimal b) {
         return a.divide(b);
     }
-
-    @Override
-    public BigDecimal abs(BigDecimal a) {
-        return a.abs();
-    }
 }

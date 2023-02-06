12ba66f1b6a14c5b23abe8ec6d9241c266ae6063
Khodakovskiy Dmitriy <khda91@gmail.com>
2023-02-06 22:05:45 +0300

    Lesson 03 - Build and Unit Tools
    - Test NG

---
+++ src/test/java/ru/levelp/at/lesson0304/hooks/suite/BeforeAfterSuiteHookSymbolDeliterNegativeTest.java
@@ -0,0 +1,31 @@
+package ru.levelp.at.lesson0304.hooks.suite;
+
+import java.util.Collections;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class BeforeAfterSuiteHookSymbolDeliterNegativeTest extends BeforeAfterSuiteHookBaseSymbolDeliterTest {
+
+    @BeforeClass
+    @Override
+    public void beforeClass() {
+        super.beforeClass();
+        System.out.println("Оп! Переопределили");
+    }
+
+    @Test(expectedExceptions = {IllegalArgumentException.class},
+          expectedExceptionsMessageRegExp = "Входные данные не могут быть пустыми")
+    public void shouldThrowExceptionWhenListIsNull() {
+        System.out.println(this.getClass().getName() + " shouldThrowExceptionWhenListIsNull");
+
+        sd.deleteSymbol(null, "S");
+    }
+
+    @Test(expectedExceptions = {IllegalArgumentException.class},
+          expectedExceptionsMessageRegExp = "Входные данные не могут быть пустыми")
+    public void shouldThrowExceptionWhenListIsEmpty() {
+        System.out.println(this.getClass().getName() + " shouldThrowExceptionWhenListIsEmpty");
+
+        sd.deleteSymbol(Collections.emptyList(), "S");
+    }
+}

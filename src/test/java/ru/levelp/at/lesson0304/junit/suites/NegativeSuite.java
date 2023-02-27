448a32552def6ee82b2ec2c1bcbfc0fb79c19718
Khodakovskiy Dmitriy <khda91@gmail.com>
2023-02-27 19:14:59 +0300

    Junit Suites how to run tests

--- src/test/java/ru/levelp/at/lesson0304/junit/suites/NegativeSuite.java
+++ src/test/java/ru/levelp/at/lesson0304/junit/suites/NegativeSuite.java
@@ -4,11 +4,11 @@ import org.junit.platform.suite.api.IncludeClassNamePatterns;
 import org.junit.platform.suite.api.IncludeTags;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
+import ru.levelp.at.lesson0304.junit.tags.TagNames;
 
 @Suite
-@IncludeTags({"positive"})
+@IncludeTags({TagNames.NEGATIVE_TAG_NAME})
 @SelectPackages("ru.levelp.at.lesson0304.junit")
 @IncludeClassNamePatterns(".*IT")
-//@SelectClasses({SymbolDeliterPositiveIT.class})
-public class PositiveSuite {
+public class NegativeSuite {
 }
